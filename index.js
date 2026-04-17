3485d31494e5f58f1e683f76b2dea6dff3e585d5
JulerGaming <124395008+JulerGaming@users.noreply.github.com>
2026-04-17 16:12:00 -0400

    commit

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(443, () => {
-    console.log('Server is running on https://localhost:443');
+app.listen(3000, () => {
+    console.log('Server is running on https://localhost:3000');
 });
 
 const { exec } = require("child_process");
