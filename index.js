0af708b372d0ca1866b1593710083043e2130150
JulerGaming <124395008+JulerGaming@users.noreply.github.com>
2026-04-17 16:14:28 -0400

    fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
     console.log('Server is running on https://localhost:3000');
 });
 
